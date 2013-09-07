478d1dcbbb1baed1af75523aa0c30ecfb26a6bc1
Jeremy Tice <jetpacmonkey@gmail.com>
2013-09-07 00:18:00 -0400

    Migration for Player model

---
+++ tracker/migrations/0002_auto__add_player.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Player'
+        db.create_table(u'tracker_player', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=60)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=60)),
+        ))
+        db.send_create_signal('tracker', ['Player'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Player'
+        db.delete_table(u'tracker_player')
+
+
+    models = {
+        'tracker.player': {
+            'Meta': {'object_name': 'Player'},
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '60'})
+        }
+    }
+
+    complete_apps = ['tracker']
